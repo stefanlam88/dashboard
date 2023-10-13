37519266f168fa2e4fa3323edfaf77f11cee2ecc
YogaSky <mmyogashri@gmail.com>
2023-10-13 13:21:46 +0800

    fix back

--- dashboard/component/MainLayout.jsx
+++ dashboard/component/MainLayout.jsx
@@ -98,11 +98,7 @@ const MainLayout = ({ children }) => {
           {Menus.map((Menu, index) => (
             <li
               key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-              ${Menu.gap ? "mt-9" : "mt-2"} ${
-                index === 0 && "bg-light-white"
-              } `}
-            >
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 ${Menu.gap ? "mt-9" : "mt-2"} ${ index === 0 && "bg-light-white"} `}>
               <img src={`./src/assets/${Menu.src}.png`} />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 {Menu.title}
