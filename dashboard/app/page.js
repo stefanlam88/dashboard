d622fc0f7406b64e6591f662ab5f11f62f5a1af2
YogaSky <mmyogashri@gmail.com>
2023-10-20 11:01:03 +0800

    added page

--- dashboard/app/page.js
+++ dashboard/app/page.js
@@ -1,14 +1,13 @@
 
 import LoginForm from "./login/page";
 import React from "react";
-import SideNavbar from "../component/SideNavbar";
 
 export default function Home() {
   return (
     <>
    <div>
-    <h1>Homepage</h1>
-    
+   
+    <LoginForm />
     </div>
     </>)
 }
